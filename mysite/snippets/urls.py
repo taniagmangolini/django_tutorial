d315cb23ea4f31363e5149ca756dfeae723fef5d
taniagmangolini <taniagmangolini@gmail.com>
2021-05-04 16:52:11 -0300

    request and response tutorial

--- mysite/snippets/urls.py
+++ mysite/snippets/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
 ]
+
+#Because the API chooses the content type of the
+# response based on the client request, it will, by default, return an HTML-formatted representation
+urlpatterns = format_suffix_patterns(urlpatterns)
