84dd45f0d93256713c1337ba3091c8850740e784
taniagmangolini <taniagmangolini@gmail.com>
2021-05-04 23:33:20 -0300

    APIView class bases

--- mysite/snippets/urls.py
+++ mysite/snippets/urls.py
@@ -2,9 +2,18 @@ from django.urls import path
 from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
+'''
+#Using @api_view(
 urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
+]
+'''
+
+#Using APIView Class
+urlpatterns = [
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
 ]
 
 #Because the API chooses the content type of the
